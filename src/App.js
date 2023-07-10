6705aa6b9d506c780bb774b03edf1be16b730fbd
Robert Putt <Robert.Putt@C02FV3BTMD6R.local>
2023-07-10 10:24:20 +0100

    Add tree view

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
+import TreeView from '@mui/lab/TreeView';
+import TreeItem from '@mui/lab/TreeItem';
+
+// Icons
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import TrafficIcon from '@mui/icons-material/Traffic';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+import WcIcon from '@mui/icons-material/Wc';
+
 mapboxgl.accessToken = 'pk.eyJ1Ijoicm9icHV0dCIsImEiOiJjbGp2dTVpMHEwam1nM3VuMXN2dHk1aWsxIn0.AwfsC_Not91E4oxcYguXhA';
 
 export default function App() {
@@ -43,12 +53,22 @@ export default function App() {
   return (
     <div>
       <div className="sidebar">
-      <ul>
-        <li><b>Transport</b></li>
-        <ul>
-            <li>Traffic <input type="checkbox" id="checkTraffic" name="checkTraffic" checked={traffic} onClick={toggleTraffic} /></li>
-        </ul>
-      </ul>
+      <TreeView
+          aria-label="file system navigator"
+          defaultCollapseIcon={<ExpandMoreIcon />}
+          defaultExpandIcon={<ChevronRightIcon />}
+          sx={{ height: 120, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
+        >
+          <TreeItem nodeId="catTransport" label="Transport">
+            <TreeItem icon={<TrafficIcon />} nodeId="itemTraffic" label="Traffic" onClick={toggleTraffic} />
+          </TreeItem>
+          <TreeItem nodeId="catHealth" label="Health">
+            <TreeItem icon={<LocalHospitalIcon />} nodeId="itemEDWaitTimes" label="Emergency Department Wait Times" />
+          </TreeItem>
+          <TreeItem nodeId="catAmenities" label="Amenities">
+            <TreeItem icon={<WcIcon />} nodeId="itemPublicToilets" label="Public Toilets" />
+          </TreeItem>
+      </TreeView>
       </div>
       <div ref={mapContainer} className="map-container" />
     </div>
